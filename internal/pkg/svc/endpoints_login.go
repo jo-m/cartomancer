bc521659c20ddf538608aa290e3697e67fc4f011
Jonathan M <jo-m@outlook.com>
2025-03-01 18:05:15 +0100

    allow both

--- internal/pkg/svc/endpoints_login.go
+++ internal/pkg/svc/endpoints_login.go
@@ -27,11 +27,18 @@ func (c *SessionCookieWriter) VisitPostApiV1LoginResponse(w http.ResponseWriter)
 	return nil
 }
 
+func getBody(r PostApiV1LoginRequestObject) Login {
+	if r.FormdataBody != nil {
+		return Login(*r.FormdataBody)
+	}
+	return Login(*r.JSONBody)
+}
+
 /*
 curl -X POST --data '{"username":"test","password":"asdf"}' -H 'content-type: application/json' http://127.0.0.1:8050/api/v1/login
 */
 func (s *Server) PostApiV1Login(ctx context.Context, request PostApiV1LoginRequestObject) (PostApiV1LoginResponseObject, error) {
 	// TODO: password check.
-	fmt.Println(request.Body)
+	fmt.Println(getBody(request))
 	return &SessionCookieWriter{value: "asdf"}, nil
 }
